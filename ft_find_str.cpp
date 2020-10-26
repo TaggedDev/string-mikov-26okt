75ad93223270cbba43665e7e1da8428995fa6dc1
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2020-10-26 09:55:50 +0300

    s
    s

--- ft_find_str.cpp
+++ ft_find_str.cpp
@@ -2,31 +2,18 @@
 
 int ft_find_str(string str1, string str2)
 {
-    int n = 0;
-    int a = 0;
+	string subs;
+	int length = ft_len(str2);
+	int nomer = -1;
 
-    for (int i = 0; i < ft_len(str1); i++) {
-        if (str1[i] == str2[n]) {
-            n += 1;
-            if (ft_len(str2) == n) {
-                for (int i = 0; str2[0] != str1[i]; i++) {
-                    a += 1;
-                }
-                return a;
-            }
+	for (int i = 0; i < ft_len(str1)-length+1; i++)
+	{
+		subs = str1.substr(i, length);
+		if (subs == str2) {
+			nomer = i;
+		}
+	}
 
-
-        }
-        else {
-            a = 0;
-            n = 0;
-        }
-    }
-    if (ft_len(str2) == n) {
-        return n;
-    }
-    else {
-        return -1;
-    }
+	return nomer;
 
 }
