6eedbe28f284724cae5d8846b142fdf5923a4586
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2020-10-26 13:39:59 +0300

    Update ft_print_copy_str.cpp

--- ft_print_copy_str.cpp
+++ ft_print_copy_str.cpp
@@ -1,7 +1,7 @@
 #include "new_str.h"
 
 using namespace std;
-string ft_print_copy_st(string str, int number)
+string ft_print_copy_str(string str, int number)
 {
 	string classic = str;
 	while (number > 1)
