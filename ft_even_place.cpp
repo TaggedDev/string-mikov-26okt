6c463024d318ff8b70a72a356ae88ad52857de06
taggeddev <denis.mikov.06@gmail.com>
2020-10-26 09:19:28 +0300

    q
    q

--- ft_even_place.cpp
+++ ft_even_place.cpp
@@ -4,7 +4,7 @@ string ft_even_place(string str)
     string newstr;
     int a = 0;
     while (a < ft_len(str)) {
-        if (a % 2 == 1) {
+        if (a % 2 == 0) {
             newstr += str[a];
         }
         a++;
